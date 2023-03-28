d92b1472a4f17f4608fe9f69ca3268b12b8ba029
Eric <ericnguyen23@gmail.com>
2023-03-27 22:45:22 -0700

    Created prompts file, connections file, and added env data

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
-const routes = require("./routes");
+// const routes = require("./routes");
+const sequelize = require("./config/connection");
+const Prompts = require("./lib/prompts");
 
 const app = express();
 
@@ -7,7 +9,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(routes);
+// app.use(routes);
+
+// start prompts
+const newPrompt = new Prompts();
+newPrompt.render();
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(3001, () => console.log("Now listening"));
